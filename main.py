61da21d0370bd68e2a27e23a6d4edacb4f402723
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-17 19:23:51 +0600

    Function to get mouse pointer coordinates when clicked on map

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
+import turtle
+from turtle import Turtle, Screen
 
+screen = Screen()
+screen.title("US States Game")
+
+image = "blank_states_img - Copy.gif"
+
+screen.addshape(image) #adding a shape to turtle
+turtle.shape(image) #using that shape
+
+#THIS FUNCTION PRINTS COORDINATES OF THE MOUSE POINTER WHEN MAP IS CLICKED: ****
+def get_mouse_click_coor(x, y):
+    print(x, y)
+turtle.onscreenclick(get_mouse_click_coor)
+turtle.mainloop()
+#,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,****
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
